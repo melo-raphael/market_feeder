ea0c972c2830983648a3fa509da753e6923fdd14
melo-raphael <rafa.psm@hotmail.com>
2020-06-14 16:01:33 -0300

    add cors

--- projeto.tcc.market.feeder.api.v1/Startup.cs
+++ projeto.tcc.market.feeder.api.v1/Startup.cs
@@ -36,6 +36,8 @@ namespace projeto.tcc.market.feeder.api.v1
                 //options.InstanceName = "Quotes:";
             });
 
+            services.AddCors();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +47,8 @@ namespace projeto.tcc.market.feeder.api.v1
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(option => option.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 
             //app.UseHttpsRedirection();
 
