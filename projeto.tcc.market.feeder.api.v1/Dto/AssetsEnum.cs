83761c953191a4f771333483626f74f9073baea6
melo-raphael <rafa.psm@hotmail.com>
2020-06-16 12:32:49 -0300

    reduzindo assets

--- projeto.tcc.market.feeder.api.v1/Dto/AssetsEnum.cs
+++ projeto.tcc.market.feeder.api.v1/Dto/AssetsEnum.cs
@@ -8,25 +8,9 @@ namespace projeto.tcc.market.feeder.api.v1.Dto
     public enum AssetsEnum
     {
         OIBR3,
-        CIEL3,
-        VVAR3,
         PETR4,
-        IRBR3,
-        COGN3,
-        BBDC4,
-        AZUL4,
-        ITUB4,
-        BBAS3,
-        GOLL4,
-        ITSA4,
         USIM5,
         VALE3,
-        HBOR3,
-        POMO4,
-        ABEV4,
-        PETR3,
-        BOVA11,
-        GGBR4
-
+        ABEV3
     }
 }
